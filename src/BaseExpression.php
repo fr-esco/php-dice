3221f347f9e4ad384362bcb40caaaef94cedb6b9
Francesco Colamonici <f.colamonici@gmail.com>
2016-05-02 12:08:48 +0200

    base expression

--- src/BaseExpression.php
+++ src/BaseExpression.php
@@ -1,15 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Francesco Colamonici
- * Date: 02/05/2016
- * Time: 12:05
- */
 
 namespace dice;
 
-
-class BaseExpression
+abstract class BaseExpression
 {
+    public $type = 'expression';
+    public $value = null;
 
+    abstract public function evaluate($scope = null);
 }
